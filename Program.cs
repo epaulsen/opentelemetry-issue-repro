2bee047901f4c537805677c7fba6abcf8b4ba046
Erling Paulsen <epa@dips.no>
2021-10-05 10:34:15 +0200

    OpenTelemetryProtocolExporter does not work as expected when targeting netcoreapp3.1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace opentelemetry_issue_repro
+{
+    class Program
+    {
+        public static ActivitySource Source = new ActivitySource("ReproSource");
+
+        static void Main(string[] args)
+        {
+            using var tracer = Sdk.CreateTracerProviderBuilder()
+                .AddSource("ReproSource")
+                .AddOtlpExporter()
+                .Build();
+            using (Source.StartActivity("Test"))
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
